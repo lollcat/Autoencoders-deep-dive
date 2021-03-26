2dc2e04c13fbbaa83aa6fdb8d90f657c7289e1ce
Laurence Midgley <loll.midgley@gmail.com>
2021-03-26 12:03:22 +0200

    add example runs for CIFAR basic and ladder

--- CIFAR_ladder/run_experiment_example.py
+++ CIFAR_ladder/run_experiment_example.py
@@ -1,12 +1,11 @@
-from CIFAR_basic_IAF.VAE import VAE
+from CIFAR_ladder.VAE import VAE_ladder as VAE
 from Utils.load_CIFAR import load_data
-from IAF_VAE_mnist.Experiment_dicts import experiment_dicts_paper # we use the same basic archetecture that we used for mnist
-
+from CIFAR_ladder.Experiment_dicts import experiment_dicts
 
 batch_size = 100
 EPOCHS = 100
 train_loader, test_loader = load_data(batch_size)
-experiment_dict = experiment_dicts_paper[3]
+experiment_dict = experiment_dicts[3]
 print(experiment_dict)
 vae = VAE(**experiment_dict)
 train_history, test_history, bits_per_dim = vae.train(EPOCHS, train_loader, test_loader, save=True,
