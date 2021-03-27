82cd7db83213072f4a16c3ff45c8e7a008b940ea
Laurence Midgley <loll.midgley@gmail.com>
2021-03-27 21:33:25 +0200

    update experiment dicts CIFAR

--- CIFAR_full_model/Experiment_dicts.py
+++ CIFAR_full_model/Experiment_dicts.py
@@ -1,8 +1,8 @@
 experiment_dicts = [
-    {"n_rungs":4, "with_IAF":False, "IAF_n_hidden_layers":1},
+    {"n_rungs":4, "with_IAF":False},
     {"n_rungs":4, "with_IAF":True, "IAF_n_hidden_layers":1},
-    {"n_rungs":8, "with_IAF":False, "IAF_n_hidden_layers":1},
+    {"n_rungs":8, "with_IAF":False},
     {"n_rungs":8, "with_IAF":True, "IAF_n_hidden_layers":1},
-    {"n_rungs":20, "with_IAF":False, "IAF_n_hidden_layers":1},
+    {"n_rungs":20, "with_IAF":False},
     {"n_rungs":20, "with_IAF":True, "IAF_n_hidden_layers":1},
                     ]
